f523cb144c57703f7199be0fbf1c781485768fa1
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2026-09-02 19:13:41 -0400

    Primer commit - Subida inicial del proyecto

---
+++ Proyecto_Arquitectura_Micromercado/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_Arquitectura_Micromercado.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
