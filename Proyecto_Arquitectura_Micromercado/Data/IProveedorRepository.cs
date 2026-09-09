84690df5aa80ac2e5cd508fca0868405292d3d13
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2026-09-09 10:13:23 -0400

    feat(data): add IProveedorRepository interface

---
+++ Proyecto_Arquitectura_Micromercado/Data/IProveedorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto_Arquitectura_Micromercado.Models;
+
+namespace Proyecto_Arquitectura_Micromercado.Data
+{
+    public interface IProveedorRepository
+    {
+        List<Proveedor> ObtenerActivos();
+        Proveedor? ObtenerPorId(int id);
+        void Insertar(Proveedor proveedor);
+        void Actualizar(Proveedor proveedor);
+        void Eliminar(int id);
+    }
+}
