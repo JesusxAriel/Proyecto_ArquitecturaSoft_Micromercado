2845574cf272331ac902d42bafd59163d40cec7c
Flora <flora.ortiz@ucb.edu.bo>
2026-09-24 01:37:02 -0400

    fix: sync methods deletion

--- Proyecto_Arquitectura_Micromercado/Application/Categories/ICategoryService.cs
+++ Proyecto_Arquitectura_Micromercado/Application/Categories/ICategoryService.cs
@@ -3,13 +3,8 @@ using Proyecto_Arquitectura_Micromercado.Domain.Categories;
 
 namespace Proyecto_Arquitectura_Micromercado.Application.Categories
 {
-    public interface ICategoryService :
+     public interface ICategoryService :
         IServicioCRUD<Category, int, Category, Category>
     {
-        List<Category> GetActive();
-        Category? GetById(int id);
-        bool Create(Category category);
-        bool Update(Category category);
-        bool Delete(int id, int adminUserId);
     }
 }
