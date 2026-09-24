c788cea47eeefaad289e158f987559d0716e261a
Flora <flora.ortiz@ucb.edu.bo>
2026-09-24 02:38:55 -0400

    feat: category name and code normalization

--- Proyecto_Arquitectura_Micromercado/Domain/Categories/CategoryDtos.cs
+++ Proyecto_Arquitectura_Micromercado/Domain/Categories/CategoryDtos.cs
@@ -9,7 +9,7 @@ namespace Proyecto_Arquitectura_Micromercado.Domain.Categories
             @"^(?=.*[A-Za-zÁÉÍÓÚáéíóúÑñ])[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s\-&]+$";
 
         public const string CodePattern =
-            @"^[A-Za-z0-9\-]+$";
+            @"^CAT-[A-Z]{3}$";
 
         public const string AislePattern =
             @"^Pasillo\s[1-8]$";
